e3aae22e9f19fa22339f9e0602d4742462340fba
Zachary Butler <254991337+Zbutler798@users.noreply.github.com>
2026-04-29 14:49:46 -0400

    added notes to skeleton function

--- mcp_server/return_functions.py
+++ mcp_server/return_functions.py
@@ -17,3 +17,13 @@ def returnFunc(type, jsonObj): # Specify what type of data you want to retrieve
 
 
 returnFunc("Email")
+
+''' Notes
+We're saving the data from AZURE; threat level, email details
+REST API data: Various aspects which are flagged
+
+User can ask:
+- List of data (emails, sender, date etc...)
+- Specific data (EX. user prompts with email, gets sender & date)
+- Prompts server? (Does the user send the data thru the MCP server to Azure?)
+'''
