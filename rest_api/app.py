c2dba7eadd5c1d5e87d6b10507d2ff688db5d981
zzJoni <Jbala4130@gmail.com>
2026-05-03 19:39:10 -0400

    Added option to generate test data rather
    - Moved app creation into a method factory named create_app
    - Added parameter to create_app to use fake data rather than querry the
      IPQS API
    - Added funtion to generate test data about email threat level that
      followed reasonable charectoristics such that a low risk email would
      not have highly suspicouse behavior

--- rest_api/app.py
+++ rest_api/app.py
@@ -2,18 +2,28 @@ from config import IPQS_API_KEY, IPQS_URL
 from flask import Flask
 import requests
 
-app = Flask(__name__)
+def create_app(debug = False, use_fake_data = False):
+    app = Flask(__name__)
 
-# Validate API key
-if IPQS_API_KEY == None or IPQS_API_KEY == "":
-    raise KeyError("IPQS API Key not provided")
-elif not requests.get(IPQS_URL + "json/account/" + IPQS_API_KEY).json().get("success"):
-    raise KeyError("Invalid IPQS API Key")
+    # Validate API key
+    if not use_fake_data:
+        if IPQS_API_KEY == None or IPQS_API_KEY == "":
+            raise KeyError("IPQS API Key not provided")
+        elif not requests.get(IPQS_URL + "json/account/" + IPQS_API_KEY).json().get("success"):
+            raise KeyError("Invalid IPQS API Key")
 
 
-# Import routes
-with app.app_context():
-    import check_email
+    # Import routes
+    with app.app_context():
+        if use_fake_data:
+            import mock_check_email
+        else:
+            import check_email
+
+    app.config["DEBUG"] = debug
+
+    return app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app = create_app(debug=True, use_fake_data= True)
+    app.run()
--- rest_api/check_email.py
+++ rest_api/check_email.py
@@ -3,36 +3,40 @@ from config import IPQS_API_KEY, IPQS_URL
 from flask import current_app as app
 import requests
 
+# Converts a fraud score int into a string reperesentation
+def _get_fraud_string(fraud_score: float) -> str:
+    match fraud_score: 
+        case None:
+            return "no_data"
+        case x if x >= 90:
+            return "malicious"
+        case x if x >= 85:
+            return "high_risk"
+        case x if x >= 75:
+            return "suspicious"
+        case x if x >= 25:
+            return "neutral"
+        case _:
+            return "low_risk"
+
+
 @app.get("/check_email/<email>")
-def check_email(email: str):
+def check_email(email: str) -> dict:
+    """Returns JSON data about the risk of the specified email"""
+    
     response: dict = requests.get(f"{IPQS_URL}json/email/{IPQS_API_KEY}/{email}").json()
 
-    # Returns early if email is valid or a problem occurs connecting to the API
+    # Returns early if email is invalid or a problem occurs connecting to the API
     if not response.get("success") or response.get("timed_out"):
         return {"success" : False, "reason" : "No valid response from IPQS API"}
     elif response.get("overall_score") == 0:
         return {"success" : False, "reason" : "Invalid email address"}
     
-    # Converts fraud score into more easily readable values
-    fraud_score: str
-    int_score = response.get("fraud_score")
-    match int_score: 
-        case x if x >= 90:
-            fraud_score = "malicious"
-        case x if x >= 85:
-            fraud_score = "high_risk"
-        case x if x >= 75:
-            fraud_score = "suspicious"
-        case x if x >= 25:
-            fraud_score = "neutral"
-        case _:
-            fraud_score = "low_risk"
-
     # Prepairs simplified response
     shared_keys = ["success", "disposable", "leaked", "recent_abuse", "risky_tld", "valid", "spf_record", "dmarc_record"]
     simplified_response = {x : response.get(x) for x in shared_keys}    
 
-    simplified_response["fraud_score"] = fraud_score
+    simplified_response["fraud_score"] = _get_fraud_string(response.get("fraud_score"))
     simplified_response["email_address_creation_date"] = response.get("first_seen").get("human")
     simplified_response["domain_creation_date"] = response.get("domain_age").get("human")
     return simplified_response
